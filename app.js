8ea3eacf7ded4758384bc03dbfadf31f514b92cc
BaeHyeonJik <bhj4060@gmail.com>
2024-09-02 21:28:37 +0900

    ''

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(
   cors({
     origin: [
       "https://port-0-blackboard-backend-f9ohr2alrkcohsg.sel5.cloudtype.app",
+      "https://web-blackbaord-frontend-f9ohr2alrkcohsg.sel5.cloudtype.app",
       "http://localhost:3000",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
