a4dfd17310bc18c8a71b00846c1c8ac1073d1f57
BaeHyeonJik <bhj4060@gmail.com>
2024-09-03 16:19:09 +0900

    ''

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors())
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
