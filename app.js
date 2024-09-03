d52a0bd9e6f216d9fc427b11753038fe3160d4e9
BaeHyeonJik <bhj4060@gmail.com>
2024-09-03 16:16:52 +0900

    ''

--- app.js
+++ app.js
@@ -1,49 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: [
-      "https://port-0-blackboard-backend-f9ohr2alrkcohsg.sel5.cloudtype.app/",
-      "https://web-blackbaord-frontend-f9ohr2alrkcohsg.sel5.cloudtype.app",
-      "http://localhost:8080",
-    ],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
-const testRoutes = require('./routes/test');
-const loginRoutes = require('./routes/login');
-const signupRoutes = require('./routes/signup');
-const getProfessorLecturesRoutes = require('./routes/getProfessorLectures');
-const getProfessorLecturePostsRoutes = require('./routes/getProfessorLecturePosts');
-const insertLecturePostRoutes = require('./routes/insertLecturePost');
-const getStudentLecturesRoutes = require('./routes/getStudentLectures');
-const getAllLecturesRoutes = require('./routes/getAllLectures');
-const insertRegisterRoutes = require('./routes/insertRegister');
-const getStudentLecturePostsRoutes = require('./routes/getStudentLecturePosts');
-const deleteRegisterRoutes = require('./routes/deleteRegister');
-const deleteLecturePostRoutes = require('./routes/deleteLecturePost');
 
-app.use("/api/test", testRoutes);
-app.use("/api/login", loginRoutes);
-app.use("/api/signup", signupRoutes);
-app.use("/api/getProfessorLectures", getProfessorLecturesRoutes);
-app.use("/api/getProfessorLecturePosts", getProfessorLecturePostsRoutes);
-app.use("/api/insertLecturePost", insertLecturePostRoutes);
-app.use("/api/getStudentLectures", getStudentLecturesRoutes);
-app.use("/api/getAllLectures", getAllLecturesRoutes);
-app.use("/api/insertRegister", insertRegisterRoutes);
-app.use("/api/getStudentLecturePosts", getStudentLecturePostsRoutes);
-app.use("/api/deleteRegister", deleteRegisterRoutes);
-app.use("/api/deleteLecturePost", deleteLecturePostRoutes);
-
+const loginRoutes = require('./routes/login')
+const signupRoutes = require('./routes/signup')
+const getProfessorLecturesRoutes = require('./routes/getProfessorLectures')
+const getProfessorLecturePostsRoutes = require('./routes/getProfessorLecturePosts')
+const insertLecturePostRoutes = require('./routes/insertLecturePost')
+const getStudentLecturesRoutes = require('./routes/getStudentLectures')
+const getAllLecturesRoutes = require('./routes/getAllLectures')
+const insertRegisterRoutes = require('./routes/insertRegister')
+const getStudentLecturePostsRoutes = require('./routes/getStudentLecturePosts')
+const deleteRegisterRoutes = require('./routes/deleteRegister')
+const deleteLecturePostRoutes = require('./routes/deleteLecturePost')
+app.use("/login", loginRoutes)
+app.use("/signup", signupRoutes)
+app.use("/getProfessorLectures", getProfessorLecturesRoutes)
+app.use("/getProfessorLecturePosts", getProfessorLecturePostsRoutes)
+app.use("/insertLecturePost", insertLecturePostRoutes)
+app.use("/getStudentLectures", getStudentLecturesRoutes)
+app.use("/getAllLectures", getAllLecturesRoutes)
+app.use("/insertRegister", insertRegisterRoutes)
+app.use("/getStudentLecturePosts", getStudentLecturePostsRoutes)
+app.use("/deleteRegister", deleteRegisterRoutes)
+app.use("/deleteLecturePost", deleteLecturePostRoutes)
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
