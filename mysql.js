58feabf76215aa693a5b4ab217942c9d2ad372bb
BaeHyeonJik <bhj4060@gmail.com>
2024-09-06 15:37:35 +0900

    ''

--- mysql.js
+++ mysql.js
@@ -7,6 +7,6 @@ const Pool = mysql.createPool({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database:  process.env.DATABASE_NAME,
-    port: '3306'
+    port: process.env.DATABASE_PORT,
 });
 module.exports = Pool;
